f96476848fd72fd46a5c3f85ed2b82f9659af290
suzan-srour <sroursuzan0@gmail.com>
2022-08-09 22:05:53 +0300

    A2

--- app.js
+++ app.js
@@ -1,67 +1,65 @@
 'use strict';
 let counter=0 ;
 const body = document.getElementsByTagName('body');
+
+
+let submit=document.getElementById('submit') ;
+let val=0;
+
 //constructor:
-const allFoods = [];
+
 function Food(id, name,type, price,) {
-    this.FoodId = id;
-    this.FoodName=name;
-    this.FoodType= type;
-    this.Price = price;
-    allFoods.push(this);
+    this.id= id;
+    this.name=name;
+    this.type= type;
+    this.price= price;
+    this.printMenu();
+} 
+
+
+Food.prototype.printMenu = function () {
+   let table=document.getElementById('table') ;
+
+    trEl = document.createElement('tr');
+    table.appendChild(trEl);
+    let tdEl = document.createElement('td');
+     tdEl.textContent = this.id;
+     trEl.appendChild(tdEl);
+     let newName = document.createElement('td');
+     newName.textContent = this.name;
+     trEl.appendChild(newName); 
+     let newType= document.createElement('td');
+     newType.textContent = this.type;
+     trEl.appendChild(newType);
+     let newPrice= document.createElement('td');
+     newPrice.textContent = this.price;
+     trEl.appendChild(newPrice);
+    trEl.classList.add('row');
+
+    } 
+    
+    
+    // store data in table
+
+//Food.id ;
+
+//submit(form);
+
+let form=document.getElementById("form1");
+form.addEventListener("submit",handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    let name = event.target.foodName.value ;
+    let type = event.targe.foodType.value ;
+    let price = event.target.price.value; 
+    let id=UniqNum() ;
+    const newFood=new Food(id,name,type,price);
+}
+
 
 function UniqNum(counter){ 
     this.FoodId=counter+1 ;
     ++counter; } 
 
-} 
-
-
-Food.prototype.printMenu = function () {
-
-    const divEl = document.createElement('div');
-    body[0].appendChild(divEl);
-
-    const pEl = document.createElement('p');
-    pEl.textContent = `Food Id': ${this.FoodId} : Food Name :${this.FoodName}  :Food Type :${this.FoodType} :${this.Price}` ;
-    divEl.appendChild(pEl);
-
-    const tableEl = document.createElement('table');
-    divEl.appendChild(tableEl);
-
-    for (let i = 0; i < this.allFoods.length; i++) {
-        let trEl = document.createElement('tr');
-        let tdEl = document.createElement('td');
-        tableEl.appendChild(trEl);
-        trEl.appendChild(tdEl);
-        tdEl.textContent = this.ingredients[i];
-    } // store data in table
-
-
-}
-          
-
-for (let i = 0; i < allFoods.length; i++) {
-    allFoods[i].printMenu();
-}
-
-
- console.log(allFoods);
-let form1 = document.getElementById("form");
-//submit(form);
-form1.addEventListener("submit", handleSubmit);
-function handleSubmit(event) {
-    event.preventDefault();
-    let name = event.target.this.FoodNameFoodName.value ;
-    let type = event.target.this.FoodType .value ;
-    let price = event.target.this.Price.value; }
-    
-    const newobj = new Food(name,type, price);
-    newobj.printMenu();
-    console.log(allFoods) ;
-
-    
-
-    
 
 
