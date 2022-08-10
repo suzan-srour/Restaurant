562d40f05e97ae15c99b96328787b71c4a35946c
suzan-srour <sroursuzan0@gmail.com>
2022-08-10 20:59:58 +0300

    A6

--- statistics.js
+++ statistics.js
@@ -4,27 +4,28 @@
 
 
 
-Food.prototype.printMenu = function () {
+let printMenu = (id,name,type,price)=> {
     let table=document.getElementById('table') ;
  
      trEl = document.createElement('tr');
      table.appendChild(trEl);
      let tdEl = document.createElement('td');
-      tdEl.textContent = this.id;
+      tdEl.textContent ="0000";
       trEl.appendChild(tdEl);
       let newName = document.createElement('td');
-      newName.textContent = this.name;
+      newName.textContent = name;
       trEl.appendChild(newName); 
       let newType= document.createElement('td');
-      newType.textContent = this.type;
+      newType.textContent = type;
       trEl.appendChild(newType);
       let newPrice= document.createElement('td');
-      newPrice.textContent = this.price;
+      newPrice.textContent = price;
       trEl.appendChild(newPrice);
      trEl.classList.add('row');
  
      } 
-     
+
+     printMenu() ;
 
 
 
@@ -32,21 +33,19 @@ Food.prototype.printMenu = function () {
     
         let retrievedData = localStorage.getItem("newFood");
         let arrayData = JSON.parse(retrievedData);
-        
-    
+        console.log(arrayData) ;
         if (arrayData != null) {
         for (let i = 0; i < arrayData.length; i++) {
-                    
-                new Food(
-                  arrayData[i].id,
+                    printMenu(arrayData[i].id,
                   arrayData[i].name,
                   arrayData[i].type,
-                  arrayData[i].price,
-                );
+                  arrayData[i].price) 
+               
+        
                 } }
         
-        for (let i = 0; i < AllFood.length; i++) {
+    
 
-          AllFood[i].printMenu(); 
-        }
-    }
+              } 
+
+              getData() ;
